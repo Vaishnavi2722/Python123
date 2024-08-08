9973d186b42619df30dd817dbe1af4846d8703d4
vaishnavi <vmali2704@gmail.com>
2024-08-08 22:28:33 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+# print("hello")
+# a=int(input("enter first no : "))
+# b=int(input("enter second no: "))
+# c=a+b
+# print(c)
+# d=a-b
+# print(d)
+light=input("light color: ")
+if(light== "red"  ):
+    print("stop")
+elif(light=="yellow"):
+    print("look")
+elif(light=="green"):
+    print("go")
+else:
+    print("light is broken")
+
